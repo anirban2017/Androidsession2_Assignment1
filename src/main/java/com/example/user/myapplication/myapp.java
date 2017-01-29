1b604f1dbc4ff5c8172a6308aa132a60adcd3aec
anirban2017 <anirbanmit1980@gmail.com>
2017-01-29 05:43:00 -0800

    Add files via upload

---
+++ src/main/java/com/example/user/myapplication/myapp.java
@@ -0,0 +1,10 @@
+
+        package com.example.user.myapplication;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+public class myapp extends AppCompatActivity {
+
+    @Override     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myapp);
+    } } 
